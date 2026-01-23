7141b32efd767e2e71e440e73c90c1023be8f0d2
VidaAbundante-TristanSuarez <vidaabundante.tristansuarez@gmail.com>
2026-01-22 21:07:28 -0300

    Add files via upload

---
+++ estado.js
@@ -0,0 +1,23 @@
+// ================= ESTADO GLOBAL =================
+export let uid = null;
+export let bibliaData = [];
+export let marcados = {};
+export let notas = {};
+
+export let size = 18;
+export let colorActual = "#fff3b0";
+
+export let resaltadorBloqueado = false;
+export let grupoActual = null;
+export let marcador = null;
+
+export let modoImagen = false;
+export let seleccionImagen = {};
+export let fondoFinal = null;
+
+export let textStyle = {
+  upper: false,
+  bold: false,
+  italic: false,
+  underline: false
+};
