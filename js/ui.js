491361379fd62b774a5978e20d61963e442a2a2e
VidaAbundante-TristanSuarez <vidaabundante.tristansuarez@gmail.com>
2026-01-25 20:01:21 -0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,16 @@
 import { state } from "./estado.js";
 import { mostrarTexto } from "./biblia.js";
 
+// ================= DOM (SE CARGA CON DEFER) =================
+
+const libroSel = document.getElementById("libro");
+const capSel = document.getElementById("capitulo");
+const texto = document.getElementById("texto");
+const titulo = document.getElementById("titulo");
+const notaBox = document.getElementById("notaBox");
+const notaTexto = document.getElementById("notaTexto");
+const loginModal = document.getElementById("loginModal");
+
 // ================= IR A SECCIONES =================
 
 window.irA = seccion => {
