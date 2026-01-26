c2e4ab4cda8af380599beaa8d144fea56e226056
VidaAbundante-TristanSuarez <vidaabundante.tristansuarez@gmail.com>
2026-01-26 08:34:00 -0300

    Delete js/dom.js

--- js/dom.js
+++
@@ -1,9 +0,0 @@
-// ================= DOM (SE CARGA CON DEFER) =================
-
-const libroSel = document.getElementById("libro");
-const capSel = document.getElementById("capitulo");
-const texto = document.getElementById("texto");
-const titulo = document.getElementById("titulo");
-const notaBox = document.getElementById("notaBox");
-const notaTexto = document.getElementById("notaTexto");
-const loginModal = document.getElementById("loginModal");
