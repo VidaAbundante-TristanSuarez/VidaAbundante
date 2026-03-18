969094dd9655697c7b449ca9937920b5fc981a01
VidaAbundante-TristanSuarez <vidaabundante.tristansuarez@gmail.com>
2026-03-17 22:19:46 -0300

    Update devocionales.js

--- devocionales.js
+++ devocionales.js
@@ -1259,7 +1259,7 @@ function devRenderFase(fase){
     const st = DEV.f1;
 
     // texto
-   const sc = 1;
+   const sc = scalePreviewF1();
    t.innerHTML = buildFase1HTML(st.size, sc);
     // ya no usamos la capa back para no romper tamaños diferentes
     if (b) b.style.display = "none";
