706f3f6c580d54b60bdf4bc6d20f07f332c95050
VidaAbundante-TristanSuarez <vidaabundante.tristansuarez@gmail.com>
2026-05-14 22:25:57 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const VA_CACHE = "vida-abundante-app-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(VA_CACHE).then((cache) => {
+      return cache.addAll([
+        "/VidaAbundante/biblia.html",
+        "/VidaAbundante/manifest.webmanifest",
+        "/VidaAbundante/img/app/icon-192.png",
+        "/VidaAbundante/img/app/icon-512.png",
+        "/VidaAbundante/img/app/preview-whatsapp.png"
+      ]);
+    }).catch(() => null)
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== VA_CACHE)
+          .map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
