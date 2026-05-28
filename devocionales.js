6de7adccc73f037c2bb18fa4312b6ac9e0b58d94
VidaAbundante-TristanSuarez <vidaabundante.tristansuarez@gmail.com>
2026-05-28 02:36:50 -0300

    Update devocionales.js

--- devocionales.js
+++ devocionales.js
@@ -1398,9 +1398,9 @@ function crearListaFuentes(fase){
    ========================================================= */
 const fondosCategorias = {
   paisajes: [
-     "./img/fondos/Paisajes/1a.jpeg",
-      "./img/fondos/Paisajes/2a.jpeg",
-      "./img/fondos/Paisajes/3a.jpeg",
+     "./img/fondos/Paisajes/1a.jpg",
+      "./img/fondos/Paisajes/2a.jpg",
+      "./img/fondos/Paisajes/3a.jpg",
       "./img/fondos/Paisajes/4a.jfif",
       "./img/fondos/Paisajes/5a.jfif",
       "./img/fondos/Paisajes/6a.jfif",
